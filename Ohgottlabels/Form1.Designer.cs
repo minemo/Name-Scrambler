6ed8b68872b579209b2c444aa05448ad458d2302
Moritz Wittki <wittki56@gmail.com>
2020-07-22 05:42:41 +0200

    First Commit ^^

---
+++ Ohgottlabels/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Ohgottlabels
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Noto Sans", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(736, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "I will destroi your pictures 4 fun";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Noto Sans", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Cyan;
+            this.label2.Location = new System.Drawing.Point(37, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(736, 65);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "I will destroi your pictures 4 fun";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(776, 149);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Oh god, bleas dont press this";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 248);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(776, 184);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(794, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(405, 424);
+            this.listBox2.TabIndex = 4;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(1205, 12);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(356, 424);
+            this.listBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1573, 446);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Image Scrambler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ Ohgottlabels/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Runtime.ExceptionServices;
+
+namespace Ohgottlabels
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string uname = Environment.UserName;
+            string Picpath = @"C:\Users\" + uname + @"\Pictures\";
+            if (Directory.Exists(Picpath))
+            {
+                string[] images = Directory.GetFiles(Picpath);
+                Random rnd1 = new Random(69);
+                Random rnd2 = new Random(rnd1.Next());
+                List<string> firsthalf = new List<string>();
+                List<string> secondhalf = new List<string>();
+                listBox1.Items.AddRange(Directory.GetFiles(Picpath));
+                for (int i=0; i<images.Count()-1; i++)
+                {
+                    string temp = images[i];
+                    string empty = "";
+                    temp = temp.Replace(".jpg", empty);
+                    temp = temp.Replace(".jpeg", empty);
+                    temp = temp.Replace(".png", empty);
+                    temp = temp.Replace(".PNG", empty);
+                    temp = temp.Replace(Picpath, empty);
+                    //Console.WriteLine(temp);
+                    int tempmiddle = temp.Length /2;
+                    //Console.WriteLine(tempmiddle);
+                    string half1 = "";
+                    string half2 = "";
+
+                    for (int b=0; b<= tempmiddle-1; b++)
+                    {   
+                        half1 += temp[b];
+                        
+                    }
+                    for (int b = tempmiddle; b < temp.Length; b++)
+                    {
+                        half2 += temp[b];
+
+                    }
+                    //Console.WriteLine(half1 +" "+ half2);
+                    firsthalf.Add(half1);
+                    secondhalf.Add(half2);
+
+                    //Enable this part only, if you want your files modified !
+
+                    //Console.WriteLine("Image "+i+" was found");
+                    //Directory.CreateDirectory(temp);
+                    //Console.WriteLine(temp);
+                    //File.Copy(images[i], copytopath);
+                    //Console.WriteLine(images.Count()-1);
+                    
+                }
+                string[] firstarr = firsthalf.OrderBy(x => rnd1.Next()).ToArray();
+                string[] secarr = secondhalf.OrderBy(x => rnd2.Next()).ToArray();
+                listBox2.Items.AddRange(firstarr);
+                listBox2.Items.AddRange(secarr);
+                //listBox3.Items.AddRange(secarr);
+                
+
+                for (int i = 0; i < images.Count() - 1; i++)
+                {
+                    string newname = firstarr[i] + secarr[i];
+                    listBox3.Items.Add(newname);
+                    if(images[i].EndsWith(".png"))
+                    {
+                        newname += ".png";
+                    }
+                    else if (images[i].EndsWith(".PNG"))
+                    {
+                        newname += ".PNG";
+                    }
+                    else if (images[i].EndsWith(".jpg"))
+                    {
+                        newname += ".jpg";
+                    }
+                    else if (images[i].EndsWith(".jpeg"))
+                    {
+                        newname += ".jpeg";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ohno");
+                        break;
+                    }
+                    newname = Picpath + newname;
+                    Console.WriteLine(images[i] + " would be named " + newname);
+                    File.Move(images[i], newname);
+                }
+
+
+            }
+            else
+            {
+                button1.BackColor = Color.FromName("red");
+                button1.Text = "U r lucky";
+            }
+            
+        }
+
+    }
+}
